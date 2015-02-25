dc350ed43ff5cf66dffd7e86fd2af8fab86ced59
Brian Peacock <bpeacock@signal.co>
2015-02-25 11:59:51 -0600

    Added a period to the data

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ $(function() {
 
   setTimeout(function() {
     window.location.reload();
-  }, Math.random()*5000*Math.sin(d.getMinutes()/60*2*Math.PI)+5000);
+  }, Math.random()*5000*Math.sin(d.getMinutes()/60*Math.PI)+5000);
 });
