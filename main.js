2442e46295470abd4b003a93e4e8f77764038331
Brian Peacock <bpeacock@signal.co>
2015-03-02 13:41:42 -0600

    Added API call

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ $(function() {
   var d = new Date(),
       t = 2000;
 
-  setTimeout(function() {
-    window.location.reload();
-  }, Math.random()*t/5 + (t*Math.sin(d.getMinutes()/60*Math.PI)+t));
+  // setTimeout(function() {
+  //   window.location.reload();
+  // }, Math.random()*t/5 + (t*Math.sin(d.getMinutes()/60*Math.PI)+t));
+
+  $.ajax({
+    url: 'http://s.thebrighttag.com/api?site=KHHSZpV&referrer=purchase&price=35',
+    type: 'POST',
+    success: function() {
+      console.log('hi');
+    }
+  });
 });
