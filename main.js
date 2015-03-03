9e882503abaf30aee6fa84914874b003262ac92a
Brian Peacock <bpeacock@signal.co>
2015-03-03 09:25:50 -0600

    Added refresh back in

--- main.js
+++ main.js
@@ -2,21 +2,23 @@ $(function() {
   var d = new Date(),
       t = 2000;
 
-  // setTimeout(function() {
-  //   window.location.reload();
-  // }, Math.random()*t/5 + (t*Math.sin(d.getMinutes()/60*Math.PI)+t));
+  setTimeout(function() {
+    window.location.reload();
+  }, Math.random()*t/5 + (t*Math.sin(d.getMinutes()/60*Math.PI)+t));
 
-  $.ajax({
-    url: 'http://s.thebrighttag.com/api'
-    data: {
-      site: 'KHHSZpV',
-      referrer: 'purchase',
-      price: 35,
-      email: 'test@test.com'
-    },
-    type: 'POST',
-    success: function() {
-      console.log('hi');
-    }
-  });
+  if(Math.random() > 0.6) {
+    $.ajax({
+      url: 'http://s.thebrighttag.com/api'
+      data: {
+        site: 'KHHSZpV',
+        referrer: 'purchase',
+        price: 35,
+        email: 'test@test.com'
+      },
+      type: 'POST',
+      success: function() {
+        console.log('hi');
+      }
+    });
+  }
 });
